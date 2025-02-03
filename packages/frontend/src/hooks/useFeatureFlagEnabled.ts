05d0537f418afdb29259416670abc5dbf45038e7
Anton Antropov <anton@smartico.ai>
2025-02-03 13:40:50 +0200

    features availability

--- packages/frontend/src/hooks/useFeatureFlagEnabled.ts
+++ packages/frontend/src/hooks/useFeatureFlagEnabled.ts
@@ -1,7 +1,7 @@
 import {
     type ApiError,
     type FeatureFlag,
-    type FeatureFlags,
+    FeatureFlags,
 } from '@lightdash/common';
 import { useQuery } from '@tanstack/react-query';
 import { useFeatureFlagEnabled as useFeatureFlagEnabledPosthog } from 'posthog-js/react';
@@ -11,8 +11,35 @@ import { lightdashApi } from '../api';
  * Thin wrapper around posthog's useFeatureFlagEnabled hook that is aware
  * of our FeatureFlags enum.
  */
-export const useFeatureFlagEnabled = (featureFlag: FeatureFlags) =>
-    useFeatureFlagEnabledPosthog(featureFlag) === true || true;
+export const useFeatureFlagEnabled = (featureFlag: FeatureFlags) => {
+    switch (featureFlag) {
+        case FeatureFlags.PassthroughLogin:
+            return true;
+        case FeatureFlags.CustomVisualizationsEnabled:
+            return true;
+        case FeatureFlags.ShowDbtCloudProjectOption:
+            return true;
+        case FeatureFlags.CustomSQLEnabled:
+            return true;
+        case FeatureFlags.UserGroupsEnabled:
+            return true;
+        case FeatureFlags.DashboardTabs:
+            return true;
+        case FeatureFlags.EnableUserTimezones:
+            return true;
+        case FeatureFlags.SemanticLayerEnabled:
+            return true;
+        case FeatureFlags.DashboardComments:
+            return true;
+        case 'ai-dashboard-summary' as FeatureFlags:
+            return false;
+        case 'scim-token-management' as FeatureFlags:
+            return false;
+        default:
+            return false;
+    }
+    //useFeatureFlagEnabledPosthog(featureFlag) === true;
+}
 
 /**
  * Use our own endpoint to get the feature flag from multiple sources.
